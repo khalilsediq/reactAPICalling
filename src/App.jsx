e9dbe6df88f5f246ca403a92dca60c48518bb89d
khalilsediq <khalilullahsadique1334@gmail.com>
2026-01-29 20:51:06 +0500

    feat: Implement product display by fetching data from fakestoreapi.

--- src/App.jsx
+++ src/App.jsx
@@ -46,19 +46,71 @@
 
 
 
+import React from 'react'
+import {useEffect, useState} from  'react'
 
+const App = () => {
+    const [products, setProducts] = useState(null)
 
+    useEffect(()=>{
+        fetch('https://fakestoreapi.com/products')
+        .then((response)=>  response.json())
+        .then((response) => {
+            console.log(response);        
+            setProducts(response)
+        } )
+        .catch((err) => {
+            console.log(err);            
+        })
+    }, [])
 
+  return (
+    <>
+    <h1 style={{
+        textAlign: 'center'
+    }} >
+        My shop
+    </h1>
+    <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        gap: '20px',
+        borderRadius: '30px',
+        border: '1px solid black',
+        padding: '3px'
+    }}>
+       
+    {products && products.map((item)=>{
+        return (
+             <div style={{
+                display: 'flex', 
+                flexDirection: 'column',
+                padding: '5px, 0px, 5px, 0px',
+            border: '2px solid yellow',
+            width: '350px',
+        }}>
+            <img style={{
+                width: '150px',
+                height: '160px',
+                alignItems: 'center',
+                textAlign: 'center',
+                alignContent: 'center',
+            }} src={item.image} alt={item.description} />
+            <h2 key={item.id} >{item.title}</h2>
+            <h3>{item.price}</h3>
+            <h4>{item.description} </h4>
+           </div> 
+        ) 
+    })}
+  
 
+    </div>
+    </>
+  )
+}
 
-
-
-
-
-
-
-
-
-
+export default App
 
 
